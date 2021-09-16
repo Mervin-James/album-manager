be761e8d6cfa32ace85cfe308892d32e1b8698c0
Akshar P <aksharpatel2001@gmail.com>
2021-09-15 21:35:42 -0400

    added to Date class

--- src/albums/Date.java
+++ src/albums/Date.java
@@ -24,7 +24,10 @@ public class Date implements Comparable<Date> {
     } //take “mm/dd/yyyy” and create a Date object
 
     public Date() {
-
+        Calendar calendar = Calendar.getInstance();
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DATE);
+        year = calendar.get(Calendar.YEAR);
     } //create an object with today’s date (see Calendar class)
 
     public Date today() {
@@ -32,17 +35,71 @@ public class Date implements Comparable<Date> {
     }
 
     public boolean isValid() {
+        if (this.year < THE_EIGHTYS) {
 
+        }
+        if (this.month == 1 || this.month == 3 || this.month == 5 || this.month == 5 || this.month == 6 || this.month == 10 ||this.month == 12) {
+            if (this.day > 31) {
+                return false;
+            }
+        }
+        else if (this.month == 4 || this.month == 5 || this.month == 9 || this.month == 11) {
+            if (this.day > 30) {
+                return false;
+            }
+        }
+        else {
+            if (this.year % 4 == 0) {
+                if (this.year % 100 == 0) {
+                    if (this.year % 400 == 0) {
+                        if (this.day > 29) {
+                            return false;
+                        }
+                    }
+                }
+                else {
+                    if (this.day > 29) {
+                        return false;
+                    }
+                }
+            }
+            else {
+                if (this.day > 28) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
     public int compareTo(Date date) {
-
+        if (this.equals(date)) {
+            return 0;
+        }
+        if (this.year < date.year) {
+            return -1;
+        }
+        if (this.year == date.year) {
+            if (this.month == date.month) {
+                if (this.day < date.day) {
+                    return -1;
+                }
+            }
+            else if (this.month < date.month) {
+                return -1;
+            }
+        }
+        return 1;
     }
 
     @Override
     public boolean equals(Object obj) {
-
+        if (obj instanceof Date) {
+            Date date = (Date) obj;
+            return (date.month == this.month && date.day == this.day && date.year == this.year);
+        }
+        return false;
     }
 
     @Override
